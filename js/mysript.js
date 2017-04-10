d8485f2920803d64809953497c0560e3569a7480
Pawel.Marcinkowski <wafel34@gmail.com>
2017-04-10 20:42:37 +0100

    Stylizing navbar + adding a carousel (beginning).

--- js/mysript.js
+++ js/mysript.js
@@ -1,2 +1,18 @@
 $(function(){
+    //variables
+    
+    //Changing the carousel image to background image
+    
+    $('.carousel .item img').each(function(){
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+        $(this).remove();
+    });
+    
+    //carousel maintance
+    
+    $('.carousel').carousel({
+        pause: null,
+        interval: false
+    });
 });
